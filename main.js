19241258c1362f953e3d3f59d5f73dd4cf423679
Alesia_Shikurova <ales2503@yandex.by>
2024-03-04 20:49:41 +0300

    feat: add new site

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //Кнопка переход со ссылкой 
 document.querySelector('.btn_1').onclick = function(e) {
-  window.location = 'https://alesia2503.github.io/CSS-generator/';
+  window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/coffee-house/';
 };
 document.querySelector('.btn_2').onclick = function(e) {
-  window.location = 'https://alesia2503.github.io/new_site/';
+  window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/hangman/';
 };
 document.querySelector('.btn_3').onclick = function(e) {
-  window.location = 'https://alesia2503.github.io/calendar/';
+  window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/nonograms/';
 };
 document.querySelector('.btn_4').onclick = function(e) {
   window.location = 'https://alesia2503.github.io/site1/';
