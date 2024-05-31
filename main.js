f4892d702433fc2b4779a8fa991912525b9d7f80
Alesia_Shikurova <ales2503@yandex.by>
2024-05-31 12:14:11 +0300

    fix: add FASTMOTORS

--- main.js
+++ main.js
@@ -2,15 +2,16 @@
 document.querySelector('.btn_1').onclick = function(e) {
   window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/coffee-house/';
 };
+document.querySelector('.btn_4').onclick = function(e) {
+  window.location = 'https://fastmotors.by/';
+};
 document.querySelector('.btn_2').onclick = function(e) {
   window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/hangman/';
 };
 document.querySelector('.btn_3').onclick = function(e) {
   window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/nonograms/';
 };
-document.querySelector('.btn_4').onclick = function(e) {
-  window.location = 'https://alesia2503.github.io/site1/';
-};
+
 document.querySelector('.btn_5').onclick = function(e) {
   window.location = 'https://alesia2503.github.io/site.2/';
 };
